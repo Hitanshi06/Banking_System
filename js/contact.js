133183e2b822785fafc9815aa749f0adc755b600
Hitanshi06 <89157860+Hitanshi06@users.noreply.github.com>
2021-08-18 23:51:50 +0530

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,13 @@
+const contactUs = (event) => {
+    const firstName = document.querySelector('#firstname');
+    const lastName = document.querySelector('#lastname');
+    const userEmail = document.querySelector('#contactEmail');
+    const userSubject = document.querySelector('#contactSubject');
+    const userMessage = document.querySelector('#contactMessage');
+    swal({
+        text: `FirstName- ${firstName.value } \nLastName-${lastName.value} \nEmail- ${userEmail.value} \nSubject- ${userSubject.value} \nMessage- ${userMessage.value}`,
+        icon: 'success'
+    });
+    [firstName.value, lastName.value, userEmail.value, userSubject.value, userMessage.value] = ['', '', '', '', ''];
+}
+
